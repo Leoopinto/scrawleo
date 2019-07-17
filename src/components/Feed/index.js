7a378e969b67a95ebe8beb1ca8ae6a5f84f69337
Leonardo Pinto <leopirespinto@gmail.com>
2019-07-17 11:10:23 -0300

    update contact button

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 
 import './styles.css'
 
-function Feed() {   
+function Feed() {
   const [items, setItems] = useState([])
 
   async function getFeed() {
     const res = await fetch(
       `https://api.instagram.com/v1/users/self/media/recent?access_token=${
-        process.env.REACT_APP_INSTAGRAM_TOKEN
+      process.env.REACT_APP_INSTAGRAM_TOKEN
       }`
     );
     const resJson = await res.json();
@@ -18,23 +18,22 @@ function Feed() {
   useEffect(() => {
     getFeed()
   }, [])
-  
+
   console.log(items)
 
- 
-  return(
-    <div>
-    {items.map(item => (
-       <article key={item.id}>
-           <a href={item.link} target="_blank"><img src={item.images.standard_resolution.url}/></a>
-           <p>{item.caption.text}{item.likes.count}</p>
-           <button>           
-               <a href={`https://api.whatsapp.com/send?phone=5515996479484&text=Olá,%20${item.user.full_name}%20gostei%20do%20${item.link},%20gostaria%20de%20um%20orçamento`} target = "_blank">CU</a>
-           </button>
 
-       </article>
-   ))}
-  </div>
+  return (
+    <div>
+      {items.map(item => (
+        <article key={item.id}>
+          <a href={item.link} target="_blank"><img src={item.images.standard_resolution.url} /></a>
+          <p>{item.caption.text}{item.likes.count}</p>
+          <button>
+            <a href={`https://api.whatsapp.com/send?phone=5515996479484&text=Olá,%20${item.user.full_name}%20gostei%20do%20${item.link},%20gostaria%20de%20um%20orçamento`} target="_blank">Solicitar Orçamento</a>
+          </button>
+        </article>
+      ))}
+    </div>
 
   )
 }
