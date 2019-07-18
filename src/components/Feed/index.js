21492649059f413f873d66d4f0bc7dfd9ead963a
Leonardo Pinto <leopirespinto@gmail.com>
2019-07-18 00:04:32 -0300

    change instagram to dribble api

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -7,8 +7,7 @@ function Feed() {
   const [shots, setShots] = useState([])
 
 
-  // https://api.dribbble.com/v2/user/shots?access_token=
-  async function getShots() {
+  async function getShots() { //Dribbble feed
     const res = await fetch(
       `https://api.dribbble.com/v2/user/shots?access_token=55fab6ccd131a75285af0bd5daa03e532489f81f6045c317184019d9a7bf7dcc`
     );
@@ -16,7 +15,7 @@ function Feed() {
     setShots(resJson)
   }
 
-  async function getFeed() {
+  async function getFeed() { //Intagram Feed
     const res = await fetch(
       `https://api.instagram.com/v1/users/self/media/recent?access_token=${
       process.env.REACT_APP_INSTAGRAM_TOKEN
@@ -46,17 +45,20 @@ function Feed() {
       </>
     )
   }
+  function formatText(o) {
+    let text = o
+    let resultado = text.substring(3, text.length - 4);
+    return resultado
+  }
 
+ 
   function renderDribbble() {
     return (
       <>
-        {shots.map(item => (
-          <article key={shots.id}>
-            <a href={shots.link} target="_blank"><img src={shots} /></a>
-            <p>{shots.caption.text}{shots.likes.count}</p>
-            <button>
-              <a href={`https://api.whatsapp.com/send?phone=5515996479484&text=Olá,%20${shots.user.full_name}%20gostei%20do%20${shots.link},%20gostaria%20de%20um%20orçamento`} target="_blank">Solicitar Orçamento!</a>
-            </button>
+        {shots.map(shot => (
+          <article key={shot.id}>
+            <img src={shot.images.hidpi} width="640px"/>
+            <p></p>
           </article>
         ))}
       </>
@@ -66,7 +68,7 @@ function Feed() {
 
   return (
     <div className="Feed">
-      {renderInsta()}
+      {/* {renderInsta()} */}
       {renderDribbble()}
     </div>
 
